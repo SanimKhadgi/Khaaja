897c6d86b9623b6d417fcc1eb25dc460eef28475
InvI <sanim46.sk@gmail.com>
2020-02-04 11:08:58 +0545

    Layout

--- app/src/main/java/com/sanim/khaaja/MainActivity.java
+++ app/src/main/java/com/sanim/khaaja/MainActivity.java
@@ -3,12 +3,20 @@ package com.sanim.khaaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp,btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+
+
     }
 }
